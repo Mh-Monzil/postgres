0d89bdda26ff9a3e914cb5f3fd77f8ae19570d42
Mh-Monzil <monzil246@gmail.com>
2025-05-18 20:58:02 +0600

    date

--- date.sql
+++ date.sql
@@ -5,3 +5,14 @@ CREATE TABLE timeZ (ts TIMESTAMP without time zone, tsz TIMESTAMP with time zone
 INSERT INTO timeZ VALUES ('2025-01-01 10:00:00', '2022-01-01 10:00:00');
 
 SELECT * FROM timeZ;
+
+SELECT CURRENT_DATE;
+
+SELECT now()::date;
+SELECT now()::time;
+
+SELECT to_char(now(), 'dd/mm/yyyy hh:mm:ss');
+
+SELECT CURRENT_DATE - INTERVAL '1 day';
+
+SELECT age(CURRENT_DATE, '2003-10-11');
